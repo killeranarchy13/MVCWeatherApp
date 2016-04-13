98c9530892af87a511d7b4efa9a2a8134901c554
R <R@R-PC>
2016-04-13 22:04:16 +0530

    DAL Added

---
+++ MVCWeatherApp/DAL/GetWeatherData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MVCWeatherApp.DAL
+{
+    public class GetWeatherData
+    {
+        public async Task<string> GetWeatherDataFromWebAPI()
+        {
+            string WeatherData;
+            using(var client=new HttpClient())
+            {
+                WeatherData = await client.GetStringAsync("api.openweathermap.org/data/2.5/weather?q={Bangalore}").ConfigureAwait(false);
+            }
+            return WeatherData;
+        }
+    }
+}
